331d68b1e136d4d812965d430399bc0d4c3787dd
Ngong kuot <ngongkuot6@gmail.com>
2025-11-10 11:17:48 +0300

    Fix CORS preflight and PathError for server.js

--- refugeechat-backend/server.js
+++ refugeechat-backend/server.js
@@ -48,7 +48,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // ✅ Handle preflight CORS requests for all routes
-app.options("*", cors(corsOptions));
+app.options("/*", cors(corsOptions));
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
