591a0f598eaa4cf9be1983e44004ad5f48a5cbe9
Ngong kuot <ngongkuot6@gmail.com>
2025-11-14 15:02:12 +0300

    Fix Tailwind class in Dashboard and add vercel rewrite config

--- refugeechat-frontend/src/pages/Dashboard.jsx
+++ refugeechat-frontend/src/pages/Dashboard.jsx
@@ -4,7 +4,8 @@ import { useIsMobile } from "../hooks/use-mobile";
 export default function Dashboard(props) {
   const isMobile = useIsMobile();
   const containerClass = isMobile
-    ? "mx-auto flex h[calc(100vh-120px)] w-full max-w-full flex-col px-2 pb-4"
+    // FIX: add missing hyphen in Tailwind class h-[...]
+    ? "mx-auto flex h-[calc(100vh-120px)] w-full max-w-full flex-col px-2 pb-4"
     : "mx-auto flex h-[calc(100vh-160px)] w-full max-w-6xl flex-col px-4 pb-8";
 
   return (
